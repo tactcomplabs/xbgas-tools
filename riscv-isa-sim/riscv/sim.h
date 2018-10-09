47824391b5b4681a2eaec80c797e4aa976d206cd
John Leidel <jleidel@tactcomplabs.com>
2018-10-09 17:24:07 +0000

    adding overloaded constructor for sim class in order to match upstream version of Stake simulator

--- riscv-isa-sim/riscv/sim.h
+++ riscv-isa-sim/riscv/sim.h
@@ -23,6 +23,11 @@ public:
   sim_t(const char* isa, size_t _nprocs,  bool halted, reg_t start_pc,
         std::vector<std::pair<reg_t, mem_t*>> mems,
         const std::vector<std::string>& args,
+	const std::vector<int> hartids,
+        unsigned progsize, unsigned max_bus_master_bits, bool require_authentication);
+  sim_t(const char* isa, size_t _nprocs,  bool halted, reg_t start_pc,
+        std::vector<std::pair<reg_t, mem_t*>> mems,
+        const std::vector<std::string>& args,
 	int world_size, int myid, int xbgas, MPI_Win win);
   ~sim_t();
 
