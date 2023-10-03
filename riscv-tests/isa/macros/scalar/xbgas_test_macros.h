ca0e424b0283c788d5d326945a09272f989196e2
Moubarak Jeje <97465738+mjeje@users.noreply.github.com>
2023-10-03 17:29:20 +0000

    esd tests

--- riscv-tests/isa/macros/scalar/xbgas_test_macros.h
+++ riscv-tests/isa/macros/scalar/xbgas_test_macros.h
@@ -14,4 +14,16 @@
 
 #define TEST_ELD_SRC1_BYPASS( testnum, nop_cycles, inst, result, offset, base, remote ) \
     SET_REMOTE_ADDR( e1, remote ) \
-    TEST_LD_SRC1_BYPASS( testnum, nop_cycles, inst, result, offset, base ) 
+    TEST_LD_SRC1_BYPASS( testnum, nop_cycles, inst, result, offset, base )
+
+#define TEST_EST_OP( testnum, load_inst, store_inst, result, offset, base, remote ) \
+    SET_REMOTE_ADDR( e1, remote ) \
+    TEST_ST_OP( testnum, load_inst, store_inst, result, offset, base )
+
+#define TEST_EST_SRC12_BYPASS( testnum, src1_nops, src2_nops, load_inst, store_inst, result, offset, base, remote ) \
+    SET_REMOTE_ADDR( e2, remote ) \
+    TEST_ST_SRC12_BYPASS( testnum, src1_nops, src2_nops, load_inst, store_inst, result, offset, base )
+
+#define TEST_EST_SRC21_BYPASS( testnum, src1_nops, src2_nops, load_inst, store_inst, result, offset, base, remote ) \
+    SET_REMOTE_ADDR( e2, remote ) \
+    TEST_ST_SRC21_BYPASS( testnum, src1_nops, src2_nops, load_inst, store_inst, result, offset, base )
